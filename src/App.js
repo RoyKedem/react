72e81cd817d02124df1b88627a3d01c2a8f2e230
roykedem <roy1kedem@gmail.com>
2021-05-08 12:44:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClippedDrawer from './ClippedDrawer';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ClippedDrawer></ClippedDrawer>
+    </div>
+  );
+}
+
+export default App;
